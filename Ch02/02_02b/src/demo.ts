7b7db2830b19e505a9175437dce97e361ca613bc
Peter Cordero <petercordero29@gmail.com>
2023-08-25 20:42:27 -0400

    done

---
+++ Ch02/02_02b/src/demo.ts
@@ -0,0 +1,21 @@
+interface Contact extends Address {
+    id: number;
+    name: string;
+    birthDate?: Date;
+}
+
+interface Address {
+    line1: string;
+    line2: string;
+    province: string;
+    region: string;
+    postalCode: string;
+}
+
+let primaryContact: Contact = {
+    birthDate: new Date("01-01-1080"),
+    id: 12345,
+    name: "Jamie Johnson",
+    postalCode: 
+}
+
---
+++ Ch06/06_04b/src/demo.ts
@@ -0,0 +1,10 @@
+interface Customer {
+    save(): void
+}
+
+class Customer {}
+
+const customer = new Customer()
+customer.save = function() {}
+
+const myVar = window.MY_VAR
